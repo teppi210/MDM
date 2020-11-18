39a43f1d3ebc6dd9e078df6f0009eb6b296f4d4d
teppi210 <phungductho@gmail.com>
2020-11-18 19:44:18 +0700

    Update mdm.sh

--- mdm.sh
+++ mdm.sh
@@ -10,3 +10,4 @@ mkdir tmp;
 mv com.apple.ManagedClient.* tmp/;
 mv com.apple.mdmclient.* tmp/;
 bless --folder /Volumes/Macintosh\ HD/System/Library/CoreServices --bootefi --create-snapshot;
+csrutil authenticated-root enable;
