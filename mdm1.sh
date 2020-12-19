735ed0f857db5077ff84e0cb83c0b19957c76a25
teppi210 <phungductho@gmail.com>
2020-12-19 10:00:07 +0700

    Create mdm1.sh

---
+++ mdm1.sh
@@ -0,0 +1,6 @@
+sudo -s
+cd ../../etc
+echo "0.0.0.0 albert.apple.com" >> hosts
+echo "0.0.0.0 iprofiles.apple.com" >> hosts
+echo "0.0.0.0 mdmenrollment.apple.com" >> hosts
+echo "0.0.0.0 deviceenrollment.apple.com" >> hosts
