b95f233405f038444d8031956cd43e97204838c5
teppi210 <phungductho@gmail.com>
2021-08-13 12:30:42 +0700

    Add files via upload

---
+++ mdmc.sh
@@ -0,0 +1,13 @@
+csrutil authenticated-root disable;
+diskutil mount /Volumes/Catalina;
+ Mount -uv /Volumes/Macintosh\ HD;	
+ cd /Volumes/Catalina/System/Library/LaunchAgents;
+mkdir tmp;
+mv com.apple.ManagedClientAgent.* tmp/;
+mv com.apple.mdmclient.* tmp/;
+cd ../LaunchDaemons;
+mkdir tmp;
+mv com.apple.ManagedClient.* tmp/;
+mv com.apple.mdmclient.* tmp/;
+bless --folder /Volumes/Catalina/System/Library/CoreServices --bootefi --create-snapshot;
+Echo "Copyright Maclife.vn nhe"
